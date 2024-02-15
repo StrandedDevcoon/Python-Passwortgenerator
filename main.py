fa3746763d7ede9ecfe721c31db290eca4ce38a6
Titas Siauciunas <siau.titas.ciunas@gmail.com>
2024-02-15 16:18:25 +0100

    [DOC] Add Documentary

--- main.py
+++ main.py
@@ -7,6 +7,15 @@ import string
 
 
 def generate_password(length, digits, upper, symbols):
+    """
+    Generate a random password.
+
+    :param length: The length of the password.
+    :param digits: A flag indicating whether to include digits in the password.
+    :param upper: A flag indicating whether to include uppercase letters in the password.
+    :param symbols: A flag indicating whether to include symbols in the password.
+    :return: The generated password as a string.
+    """
     chars = string.ascii_lowercase
     if digits:
         chars += string.digits
@@ -18,6 +27,33 @@ def generate_password(length, digits, upper, symbols):
 
 
 class MainWindow(QMainWindow):
+    """
+
+    The `MainWindow` class represents the main window of a password generator application. It inherits from the
+    `QMainWindow` class.
+
+    Attributes:
+        - `length_label`: QLabel - The label for the password length input field.
+        - `length_var`: QLineEdit - The input field for the password length.
+        - `digits_var`: QCheckBox - The checkbox for including digits in the generated password.
+        - `upper_var`: QCheckBox - The checkbox for including uppercase letters in the generated password.
+        - `symbols_var`: QCheckBox - The checkbox for including symbols in the generated password.
+        - `result_label`: QLabel - The label for the generated password.
+        - `result_var`: QLineEdit - The input field for the generated password.
+        - `generate_button`: QPushButton - The button for generating a password.
+        - `copy_button`: QPushButton - The button for copying the generated password to the clipboard.
+
+    Methods:
+        - `__init__()`: Initializes the `MainWindow` object and sets up the UI.
+        - `generate_password_clicked()`: Generates a password based on the user's input and sets it as the generated
+            password.
+        - `copy_to_clipboard()`: Copies the generated password to the clipboard.
+
+    Example usage:
+        main_window = MainWindow()
+        main_window.show()
+
+    """
     def __init__(self):
         super(MainWindow, self).__init__()
 
@@ -74,6 +110,22 @@ class MainWindow(QMainWindow):
 
 
 def main():
+    """
+    Main Method
+    ------------
+    Starts the application and creates a main window.
+
+    Returns
+    -------
+    None
+
+    Example
+    -------
+    To use this method, simply call it:
+
+        main()
+
+    """
     app = QApplication(sys.argv)
     win = MainWindow()
     win.show()
